0fb619da7dd7c7c4862d21886c09892aee624802
AmbiShanthi19 <ambishanthi19@gmail.com>
2025-01-11 00:09:05 +0530

    Programs of Task5

---
+++ Program7.java
@@ -0,0 +1,26 @@
+
+
+package task5;
+
+public class Program7 {
+
+	public static void main(String[] args) {
+		int a= 12345;
+		int b= 23456;
+		int c = 1235;
+		if ((a>b) && (a>c))
+		{
+			System.out.println("A is the largest number");
+		}
+		else if ((b>a) && (b>c))
+		{
+			System.out.println("B is the largest number");
+		}
+		else if ((c>b) && (c>b))
+		{
+			System.out.println("c is the largest number");
+		}
+						
+	}
+
+}
